36f78ad3e49937ca1da7cec96b22658c13af573c
Tejas Mehetre <85450083+tejasvm123@users.noreply.github.com>
2024-07-22 10:53:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,67 +1,60 @@
 $(document).ready(function(){
     $(window).scroll(function(){
-        // sticky navbar on scroll script
         if(this.scrollY > 20){
             $('.navbar').addClass("sticky");
-        }else{
+        } else {
             $('.navbar').removeClass("sticky");
         }
         
-        // scroll-up button show/hide script
+        // Scroll-up button show/hide
         if(this.scrollY > 500){
             $('.scroll-up-btn').addClass("show");
-        }else{
+        } else {
             $('.scroll-up-btn').removeClass("show");
         }
     });
 
-    // slide-up script
     $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
-        // removing smooth scroll on slide-up button click
+        $('html').animate({scrollTop: 0}, 'slow');
+      
         $('html').css("scrollBehavior", "auto");
     });
 
     $('.navbar .menu li a').click(function(){
-        // applying again smooth scroll on menu items click
+       
         $('html').css("scrollBehavior", "smooth");
     });
 
-    // toggle menu/navbar script
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
-    //typing animation script
+
     var typed = new Typed(".typing", {
-        strings: ["Competitive Programmer", "Web Developer", "Tech Enthusiast", "Problem Solver"],
+        strings: ["Competitive Programmer", "Web Developer", "Tech Enthusiast", "Problem Solver", "Data Science Enthusiast"],
         typeSpeed: 100,
         loop: true,
-        backspeed: 60,
+        backSpeed: 60,
         backDelay: 1000,
         startDelay: 1000,
-        showCursor: false,
-        cursorChar: "|",
-        cursorBlink: true,
-        loopCount: Infinity
+        showCursor: false
     });
-    // owl carousel script
     $('.carousel').owlCarousel({
         margin: 20,
         loop: true,
         autoplay: true,
-        autoplayTimeOut: 2000,
+        autoplayTimeout: 2000,
         autoplayHoverPause: true,
         responsive: {
-            0:{
+            0: {
                 items: 1,
                 nav: false
             },
-            600:{
+            600: {
                 items: 2,
                 nav: false
             },
-            1000:{
+            1000: {
                 items: 3,
                 nav: false
             }
